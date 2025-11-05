7509a18a8cbf7fb7770dc39f28519efc79d74b19
matthew <g00430349@atu.ie>
2025-11-05 13:06:08 +0000

    created week7 project

---
+++ src/main/java/atu/ie/cicdweek7/Cicdweek7Application.java
@@ -0,0 +1,13 @@
+package atu.ie.cicdweek7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicdweek7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicdweek7Application.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/cicdweek7/Cicdweek7ApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.cicdweek7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicdweek7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
